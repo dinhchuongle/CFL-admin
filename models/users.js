285f56ab746c0527697cf1c70811f408c210abcc
dinhchuongle <dinhchuongle@gmail.com>
2025-04-26 01:56:01 +0700

    Update users.js

--- models/users.js
+++ models/users.js
@@ -1,11 +1,17 @@
-const bcrypt = require("bcrypt");
-
-const users = [
-    {
-        username: "admin",
-        password: bcrypt.hashSync("admin123", 10),
-        role: "admin"
-    }
+const fakeUsers = [
+  { id: "1", username: "admin", password: "123456", role: "admin" },
+  { id: "2", username: "teacher", password: "abc123", role: "teacher" }
 ];
 
-module.exports = { users };
+function findByUsername(username) {
+  return Promise.resolve(fakeUsers.find(u => u.username === username));
+}
+
+function findById(id) {
+  return Promise.resolve(fakeUsers.find(u => u.id === id));
+}
+
+module.exports = {
+  findByUsername,
+  findById
+};
