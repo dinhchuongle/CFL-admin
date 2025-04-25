983aa93e68e9103d494211e3ee31dee67c41e20e
dinhchuongle <dinhchuongle@gmail.com>
2025-04-26 01:49:13 +0700

    index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("index", { user: req.user });
+  res.redirect("/auth/login");
 });
 
 module.exports = router;
